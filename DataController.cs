3f82f3c04f4ed6fd6bfd40336f10acc1076d5068
bluewood-truth <bluewood.truth@gmail.com>
2020-08-30 23:07:40 +0900

    프로젝트 시작
    유니티 프로젝트를 작성하고 DataController 싱글톤 및 JSON파일 읽기&쓰기 기능을 구현함.

---
+++ DataController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+
+public class DataController : MonoBehaviour
+{
+    public static DataController instance = null;
+    void Singleton()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Awake()
+    {
+        BetterStreamingAssets.Initialize();
+        Singleton();
+
+        Test();
+    }
+
+    void Test()
+    {
+        int[] asd = { 1, 2, 3, 4, 5 };
+        Save(asd, "ASD");
+
+        var qwe = Load<int[]>("ASD");
+        Debug.Log(qwe[0]);
+    }
+
+    // 상수
+    const string SAVE = "SAVE";
+    const string f_json = "{0}.json";
+
+
+
+
+
+    void Save(object _data, string _filename)
+    {
+        string jsonData = JsonConvert.SerializeObject(_data);
+        string path = Path.Combine(Application.persistentDataPath, SAVE);
+
+        // 세이브 폴더가 없으면 만든다
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        path = Path.Combine(path, string.Format(f_json, _filename));
+        File.WriteAllText(path, jsonData, System.Text.Encoding.UTF8);
+    }
+
+    public T Load<T>(string _filename)
+    {
+        string path = Path.Combine(Application.persistentDataPath, SAVE, string.Format(f_json, _filename));
+        string jsondata = File.ReadAllText(path);
+        return JsonConvert.DeserializeObject<T>(jsondata);
+    }
+}
